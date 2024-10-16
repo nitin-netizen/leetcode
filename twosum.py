a6a391d7756671dbaa3854087994a081e65cc1f0
TinTin <nitin@TinTins-MacBook-Pro.local>
2024-10-16 00:50:51 -0700

    twoSum: added line 2 comment

--- twosum.py
+++ twosum.py
@@ -1,15 +1,16 @@
 class Solution(object):
-   def twoSum(self, nums, target):
-       num_dict = {}
+    # We use a dictionary num_dict to store the values we have seen and their corresponding indices.
+    def twoSum(self, nums, target):
+        num_dict = {}
 
-
-       for i, num in enumerate(nums):
-           complement = target - num
-           if complement in num_dict:
-               return [num_dict[complement], i]
-           num_dict[num] = i
-       """
-       :type nums: List[int]
-       :type target: int
-       :rtype: List[int]
-
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]
+            num_dict[num] = i
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
